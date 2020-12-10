bc906a0fa4eac1105ca548df5b5f883a22459216
ohmozi <gown10195@gmail.com>
2020-12-10 23:31:56 +0900

    android API_1 - prepare UI Page

--- AndroidAPI/app/src/main/java/com/example/androidapi/MainActivity.kt
+++ AndroidAPI/app/src/main/java/com/example/androidapi/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.androidapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        val TAG: String = "로그"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "MainActivity - onCreate() called")
+
+
+
+    }
+
+    private var backKeyPressedTime: Long = 0
+    private lateinit var toast: Toast
+
+    override fun onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis()
+            toast = Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT)
+            toast.show()
+            return
+        }
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finish()
+            toast.cancel()
+        }
     }
 }
